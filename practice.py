f13e0c3171ce9e960f91184ab20f00bfcdcb53fd
Anwesh43 <anweshthecool0@gmail.com>
2019-01-17 11:34:20 +0530

    filtering dataframe to get vaulable insights

--- practice.py
+++ practice.py
@@ -1,9 +1,9 @@
 import pandas as pd
 players = {
-    "Key Passes" : [10, 9, 15, 13, 23],
+    "keyp" : [10, 9, 15, 13, 23],
     "shots" : [7, 9, 8, 12, 6],
     "goals" : [2, 1, 1, 8, 3],
-    "ball wins" : [33, 22, 5, 11, 17]
+    "bws" : [33, 22, 5, 11, 17]
 }
 df = pd.DataFrame(players)
 df.index = ["Lucas Torreira", "Matteo Guendouzi", "Mesut Ozil", "Aaron Ramsey", "Granit Xhaka"]
@@ -16,14 +16,14 @@ print(df.columns)
 print("--------")
 print("________")
 
-kp_of_all = df.loc[:,["Key Passes"]]
+kp_of_all = df.loc[:,["keyp"]]
 print("key passes of all players")
 print(kp_of_all)
 
 print("--------")
 print("________")
 
-bw_of_all = df.loc[:, ["ball wins"]]
+bw_of_all = df.loc[:, ["bws"]]
 print("ball wins of all players")
 print(bw_of_all)
 
@@ -39,3 +39,16 @@ print("________")
 mo_stats = df.loc[["Mesut Ozil"]]
 print("Mesut Ozil stats")
 print(mo_stats)
+
+
+ball_winners = df[df["bws"] > 20]
+print("ball winners are")
+print(ball_winners)
+
+goal_scorers = df[df["goals"] >= 3]
+print("top gaol scorers are ")
+print(goal_scorers)
+
+shooters = df[df["shots"] >= 8]
+print("shooters are")
+print(shooters)
